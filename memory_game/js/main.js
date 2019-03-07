c8187778906675aa27b0d61929b056ce8761d9df
Matt Talamantes <matthew@Matts-MacBook-Pro.local>
2019-03-07 12:30:44 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = [0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+var cardTwo = [1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] = cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
