0fb8da03524390af82101112fc12e8a1e3ffff75
Matt Talamantes <matthew@Matts-MacBook-Pro.local>
2019-03-07 14:32:05 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,22 @@
+// These two variables will have global scale
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = [0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		}
+};
 
-var cardTwo = [1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
 
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] = cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
 	}
-}
+};
+flipCard(0);
+flipCard(2);
